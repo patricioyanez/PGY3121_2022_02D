ee236742dcfe0347803f07c7201aeccfdb3dfda3
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-05-31 18:04:42 -0400

    update

--- EA3/proyecto2/crud/urls.py
+++ EA3/proyecto2/crud/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
         path('marca', views.marcaView, name="marca"),
         path('categoria', views.categoriaView, name="categoria"),
+        path('categoria/<int:id>/', views.categoriaLeerView, name="categoria"),
             ]
--- EA3/proyecto2/crud/views.py
+++ EA3/proyecto2/crud/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+from crud.models import Categoria
+
 # Create your views here.
 
 def marcaView(request):
     return render(request, 'marca.html', {'nombre': 'constanza'})
+
+def categoriaLeerView(request, id):
+    contexto = {}
+    if not id is None:
+        fila = Categoria.objects.get(pk = id)
+        contexto = {'fila': fila}
+
+    return render(request, 'categoria.html', contexto)
 
 def categoriaView(request):
     contexto = {}
@@ -17,14 +27,20 @@ def categoriaView(request):
 
         # detectar que botón fue presionado
 
-       # if 'btnGrabar' in request.POST:
-        #    if id < 1: # ORM Object relational Mapping
-                
-
+        if 'btnGrabar' in request.POST:
+            if id < 1: # ORM Object relational Mapping
+                Categoria.objects.create(nombre = nombre, activo = activo) 
+            else:
+                item = Categoria.objects.get(pk = id)
+                item.nombre = nombre
+                item.activo = activo
+                item.save()     
+            contexto = {'mensaje': 'Los datos fueron guardados'}
 
         
-        #elif 'btnListar' in request.POST:
-        
+        elif 'btnListar' in request.POST:
+            listado = Categoria.objects.all()
+            contexto = {'listado': listado }
 
 
-    return render(request, 'categoria.html', {'nombre': 'constanza'})
+    return render(request, 'categoria.html', contexto)
