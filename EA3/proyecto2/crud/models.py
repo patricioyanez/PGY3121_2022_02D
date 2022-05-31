ab3ddd8349ab2c28b1fb79d87c0920e148385d55
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-05-31 17:13:17 -0400

    updatre

--- EA3/proyecto2/crud/models.py
+++ EA3/proyecto2/crud/models.py
@@ -1,7 +1,15 @@
 from django.db import models
-
+# al crear un modelo se debe ejecutar los comandos 
+# makemigrations y migrate
 # Create your models here.
 class Marca(models.Model):
+    nombre = models.TextField(max_length=100)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
     nombre = models.TextField(max_length=100)
     activo = models.BooleanField()
 
--- EA3/proyecto2/crud/views.py
+++ EA3/proyecto2/crud/views.py
@@ -6,4 +6,25 @@ def marcaView(request):
     return render(request, 'marca.html', {'nombre': 'constanza'})
 
 def categoriaView(request):
+    contexto = {}
+    if request.method == 'POST':
+        # capturar datos del form
+        id = int("0" + request.POST["txtId"])
+        nombre = request.POST["txtNombre"]
+        activo = False
+        if 'chkActivo' in request.POST:
+            activo = True
+
+        # detectar que botón fue presionado
+
+       # if 'btnGrabar' in request.POST:
+        #    if id < 1: # ORM Object relational Mapping
+                
+
+
+        
+        #elif 'btnListar' in request.POST:
+        
+
+
     return render(request, 'categoria.html', {'nombre': 'constanza'})
