07113fdf811ecd38fad09e6bcdb4f5c82d403dca
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-06-08 13:57:54 -0400

    API REST FRAMEWork

---
+++ EA3/proyecto2/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from api.views import apiCategoria
+
+urlpatterns = [
+    path('apiCategoria', apiCategoria, name='apiCategoria'),
+]
--- EA3/proyecto2/proyecto2/urls.py
+++ EA3/proyecto2/proyecto2/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crud/', include('crud.urls'))
+    path('crud/', include('crud.urls')),
+    path('api/', include('api.urls'))
 ]
